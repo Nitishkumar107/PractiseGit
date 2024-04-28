309decb8926a3895a0d5c25209d00fe029c36ad8
Nitish Kumar <55441845+Nitishkumar107@users.noreply.github.com>
2024-04-28 15:42:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ def addition(a, b,c):
     return a + b+c
 
 
-def substitution(a, b,d):
-    return a - b - d 
+def substitution(a, b,e):
+    return a - b - e 
 
 def division(a, b):
     return a / b
